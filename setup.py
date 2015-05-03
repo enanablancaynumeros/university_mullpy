620a8766fb573e366897a7ac20fd43eb3891af2e
enanablancaynumeros <yeray.alvarez.romero@gmail.com>
2015-05-03 12:12:52 +0100

    gitignore

--- setup.py
+++ setup.py
@@ -82,13 +82,13 @@ setup(
     extras_require={
         'dev': ['check-manifest'],
         'test': ['coverage'],
-    },
+    }
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
-    entry_points={
-        'console_scripts': [
-            'sample=sample:main',
-        ],
-    },
+    # entry_points={
+    #     'console_scripts': [
+    #         'sample=sample:main',
+    #     ],
+    # },
 )
